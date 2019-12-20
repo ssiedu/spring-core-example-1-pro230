f6921a5561a4f7c394a5de7d367c9202743e14c2
manoj <manoj.sarwate@ssiedu.in>
2019-12-20 15:55:18 +0530

    20-12-19

--- src/main/java/com/ssi/Demo.java
+++ src/main/java/com/ssi/Demo.java
@@ -1,6 +1,9 @@
 package com.ssi;
 
 public class Demo {
+	public Demo(){
+		System.out.println("Demo Instance Created");
+	}
 	public void show(){
 		System.out.println("Something to Learn Spring, Calling Show of Demo");
 	}
--- src/main/java/com/ssi/DemoUser.java
+++ src/main/java/com/ssi/DemoUser.java
@@ -6,23 +6,19 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class DemoUser {
 
 	public static void main(String[] args) {
-
-		//we need to call the show method of Demo class
-		//we need an instance of Demo
-		
 		System.out.println("Starting Spring Application : Its Main of DemoUser ");
-		//Demo ob=new Demo();
-		//I will ask spring to provide me the instance of Demo class.
+		//ApplicationContext context=new FileSystemXmlApplicationContext("e:/spring-conf/spring.xml");
+		ApplicationContext context=new ClassPathXmlApplicationContext("application-context.xml");
+		//System.out.println("We haven't demanded d1 yet");
 		
+		System.out.println("Demanding d1 first time ==============>");
+		Demo ob1=context.getBean("d1",Demo.class);
 		
-		ApplicationContext context=new ClassPathXmlApplicationContext("spring.xml");
-		Demo ob1=(Demo) context.getBean("d1");
-		ob1.show();
+		System.out.println("Demanding d1 second time ==============>");
+		Demo ob2=context.getBean("d1",Demo.class);
 		
-		//Test t=new Test();
-		Test t=(Test)context.getBean("t1");
-		t.display();
-
+		//System.out.println(ob1==ob2);
+		
 	}
 
 }
